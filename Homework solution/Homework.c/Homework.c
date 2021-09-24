06ccb80713161ab92ba3ebaadf10eac45e4ba9fe
StakhovDmitry <Dima.I2002@mail.ru>
2021-09-24 06:47:12 +0300

    myhomework

---
+++ Homework solution/Homework.c/Homework.c
@@ -0,0 +1,36 @@
+﻿#include <stdio.h>
+
+
+int main()
+{
+    char num;
+    while (
+        (num = getchar()) != EOF &&
+        num != '\n' &&
+        num != '\0')
+    {
+        switch (num)
+        {
+        case '0':
+        case '1':
+        case '2':
+        case '3':
+        case '4':
+        case '5':
+        case '6':
+        case '7':
+        case '8':
+        case '9':
+            printf("%c", num);
+            break;
+        case '|':
+            printf(".");
+            break;
+        default:
+            printf("\n\n error \n\n");
+            exit(-1);
+        }
+    }
+    printf("\n");
+    exit(0);
+}
