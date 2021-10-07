1575ee0cf90713ad8692390394001805df5e8fea
StakhovDmitry <Dima.I2002@mail.ru>
2021-10-07 21:22:27 +0300

    Update Homework.c

--- Homework solution/Homework.c/Homework.c
+++ Homework solution/Homework.c/Homework.c
@@ -1,28 +1,31 @@
 ﻿#include <stdio.h>
 
+#include <stdlib.h>
 
-int main()
-{
-    char num, array[64];
+#include <stdint.h>
+
+#define ARRAY_LEN 64
+int main() {
+    char num, array[ARRAY_LEN];
     int k = 0;
     while (
         (num = getchar()) != EOF &&
         num != '\n' &&
-        num != '\0')
-    {
-        switch (num)
-        {
+        num != '\0') {
+        switch (num) {
         case '-':
-            if (k == 0)
-            {
+            if (k == 0) {
                 array[k] = num;
             }
-            else
-            {
-                printf("\n\n error \n\n");
+            else {
+                printf("\nInput error");
                 exit(-1);
             }
             break;
+
+        case ' ':
+            break;
+
         case '0':
         case '1':
         case '2':
@@ -34,21 +37,25 @@ int main()
         case '8':
         case '9':
             array[k] = num;
+            k++;
             break;
+
         case '|':
             array[k] = '.';
+            k++;
             break;
+
         default:
-            printf("\n error \n");
+            printf("\nInput error");
             exit(-1);
-            k = k++;
+        }
+
+        if (k >= ARRAY_LEN) {
+            printf("\nInput error");
+            exit(-1);
         }
     }
-    for (int i = 0; i <= 63; i++)
-    {
-        printf("%c", array[i]);
-    }
-    printf("\n");
+    array[k] = '\0';
+    printf("%s", array);
     exit(0);
 }
-
